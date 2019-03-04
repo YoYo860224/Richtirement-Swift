347b9cf6d9c760473acd8b80c2319943779acc91
YoCheng <xyox860224@hotmail.com>
2019-03-05 00:00:45 +0800

    Use CocoaPods.

---
+++ Richtirement/Richtirement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Richtirement
+//
+//  Created by 陳祐丞 on 2019/3/4.
+//  Copyright © 2019 陳祐丞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
