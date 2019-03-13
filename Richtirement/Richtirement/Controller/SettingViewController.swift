90e8e1dd9cbe695672d19177f1665aa2f7cb6ac1
YoCheng <xyox860224@hotmail.com>
2019-03-13 13:13:31 +0800

    SettingView ViewOver.

--- Richtirement/Richtirement/Controller/SettingViewController.swift
+++ Richtirement/Richtirement/Controller/SettingViewController.swift
@@ -12,6 +12,8 @@ class SettingViewController: UIViewController {
 
     @IBOutlet weak var progressBarOuter: UIView!
     @IBOutlet weak var progressBarInner: UIView!
+    @IBOutlet weak var progressBarTrCn: NSLayoutConstraint!
+    @IBOutlet var top4Label: [UILabel]!
     
     @IBOutlet weak var qScrollView: UIScrollView!
     @IBOutlet weak var nameTF: UITextField!
@@ -19,11 +21,17 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var femaleRdBtn: RadioBtn!
     @IBOutlet weak var maleRdBtn: RadioBtn!
     
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderValue: UILabel!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var confirmBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
     }
-    
+
     func setUI() {
         progressBarOuter.layer.cornerRadius = 2.5
         progressBarOuter.layer.masksToBounds = true
@@ -39,15 +47,61 @@ class SettingViewController: UIViewController {
         maleRdBtn.check()
         maleRdBtn.checkGroup = [femaleRdBtn]
         femaleRdBtn.checkGroup = [maleRdBtn]
+        
+        qScrollView.delegate = self
+        
+        let progbarWidth = progressBarOuter.frame.width
+        progressBarTrCn.constant = progbarWidth * 0.18
+        
+        confirmBtn.isHidden = true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func SliderDrag(_ sender: Any) {
+        // thumb size is 57
+        sliderValue.text = String(Int(5000 * slider.value))
+        sliderValue.center.x = CGFloat(Float(slider.frame.minX + 14.25) + Float(slider.frame.width - 28.5) * slider.value)
     }
-    */
+}
 
+extension SettingViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(pageControl.numberOfPages), height: 0)
+        pageControl.currentPage = Int(pageNumber)
+        
+        for (i, label) in top4Label.enumerated() {
+            if i == Int(pageNumber) {
+                UIView.transition(with: label, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                    label.textColor = UIColor(red: 255/255, green: 216/255, blue: 113/255, alpha: 1.0)
+                }, completion: nil)
+            }
+            else {
+                UIView.transition(with: label, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                    label.textColor = UIColor(red: 96/255, green: 96/255, blue: 96/255, alpha: 1.0)
+                }, completion: nil)
+            }
+        }
+        
+        let pageRatio = scrollView.contentOffset.x / scrollView.frame.size.width
+        let pageRatioInt = Int(pageRatio)
+        let pageRationFloat = pageRatio - CGFloat(pageRatioInt)
+        
+        let widRate:[CGFloat] = [0.18, 0.4, 0.7, 1]
+        let progbWidth = progressBarOuter.frame.width
+        if pageRatioInt < 3 {
+            progressBarTrCn.constant = (widRate[pageRatioInt] + (widRate[pageRatioInt + 1] - widRate[pageRatioInt]) * pageRationFloat) * progbWidth
+            
+            if !self.confirmBtn.isHidden {
+                print("x")
+                UIView.transition(with: confirmBtn, duration: 0.3, options: .curveEaseInOut, animations: {
+                    self.confirmBtn.isHidden = true
+                }, completion: nil)
+            }
+        }
+        else {
+            UIView.transition(with: confirmBtn, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                self.confirmBtn.isHidden = false
+            }, completion: nil)
+        }
+    }
 }
--- Richtirement/Richtirement/View/MyUIKit/RadioBtn.swift
+++ Richtirement/Richtirement/View/MyUIKit/RadioBtn.swift
@@ -50,3 +50,57 @@ class RadioBtn: UIView {
         initSelf()
     }
 }
+
+class SelectBtn: RadioBtn {
+    override func initSelf() {
+        super.initSelf()
+        
+        self.layer.backgroundColor = UIColor(red: 96/255, green: 96/255, blue: 96/255, alpha: 1.0).cgColor
+        self.layer.cornerRadius = self.frame.height / 3
+        if let sublabel = self.subviews[0] as? UILabel {
+            sublabel.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        }
+    }
+    
+    @objc override func targetViewDidTapped(_ sender: Any) {
+        if checkState {
+            uncheck()
+        }
+        else {
+            check()
+        }
+    }
+    
+    override func check() {
+        // Using UIView.transition is more strong than animate.
+        // But I has Fixed layerBG by using ainmate, that also a useful way.
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.layer.backgroundColor = UIColor(red: 255/255, green: 216/255, blue: 113/255, alpha: 1.0).cgColor
+        })
+        
+        if self.subviews.count > 0 {
+            if let sublabel = self.subviews[0] as? UILabel {
+                UIView.transition(with: sublabel, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                    sublabel.textColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0)
+                }, completion: nil)
+            }
+        }
+        checkState = true
+    }
+    
+    override func uncheck() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.layer.backgroundColor = UIColor(red: 96/255, green: 96/255, blue: 96/255, alpha: 1.0).cgColor
+        })
+        
+        if self.subviews.count > 0 {
+            if let sublabel = self.subviews[0] as? UILabel {
+                UIView.transition(with: sublabel, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                    sublabel.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+                }, completion: nil)
+            }
+        }
+        checkState = false
+    }
+}
