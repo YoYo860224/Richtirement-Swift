2e90c0b91245f7b6ad51a3ad22304ba1b2c04ec3
YoCheng <xyox860224@hotmail.com>
2019-03-16 23:27:50 +0800

    Game View and More image.

---
+++ Richtirement/Richtirement/Controller/MainGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainGameViewController.swift
+//  Richtirement
+//
+//  Created by 陳祐丞 on 2019/3/16.
+//  Copyright © 2019 陳祐丞. All rights reserved.
+//
+
+import UIKit
+
+class MainGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
