641ef433d7090656b65c7fec6b5a9c73b24d02f6
YoCheng <xyox860224@hotmail.com>
2019-03-20 23:52:32 +0800

    setting.

--- Richtirement/Richtirement/Model/Setting.swift
+++ Richtirement/Richtirement/Model/Setting.swift
@@ -8,24 +8,62 @@
 
 import Foundation
 
-//class SystemSetting {
+class SystemSetting {
+    static let AnnuityMax = 50;              // 年金每年最多增加多少
+    static let MedicineInsuranceMax = 50;    // 醫療保險每年最多增加多少
+    
+    static let nowPlayer = ""
+    static let Players: [PlayerData] = []
+}
+
+class PlayerData: Codable {
+    // 基本人設
+    var name: String = ""
+    var age:Int = 55
+    var gender: Int = 1                 // 1 is male, 0 is female
+
+    var hasParent: Bool = false
+    var hasFriend: Bool = false
+    var hasCouple: Bool = false
+    var hasChilds: Bool = false
+    
+    // 四大數值
+    var money = 100
+    var phychological = 75
+    var healthy = 75
+    var social = 75
+    
+    
+    var deposit: Int = 1500             // 存款
+    var stock: Int = 0                  // 股票
+    var fund: Int = 0                   // 基金
+    var annuity: Int = 0                // 年金
+    var medicineInsurance: Int = 0      // 醫療險
+//    func save() {
+//        let fm = FileManager()
+//        let path = NSHomeDirectory() + "/tmp/" + self.name + ".json"
+//        if !fm.fileExists(atPath: path) {
+//            fm.createFile(atPath: path, contents: nil, attributes: nil)
+//        }
 //
+//        let url = URL(fileURLWithPath: path)
 //
-//}
+//        if let data = try? JSONEncoder().encode(self) {
+//            try? data.write(to: url)
+//        }
+//    }
 //
-//class PlayerData {
-//    // 基本人設
-//    var name: String
-//    var age:Int
-//    var gender: Int
+//    func load() {
+//        let fm = FileManager()
+//        let path = NSHomeDirectory() + "/tmp/" + self.name + ".json"
+//        if !fm.fileExists(atPath: path) {
+//            fm.createFile(atPath: path, contents: nil, attributes: nil)
+//        }
 //
-//    var parent: Bool
-//    var friend: Bool
-//    var child: Bool
-//    var couple: Bool
+//        let url = URL(fileURLWithPath: path)
 //
-//    var save: Int       // 存款
-//
-//    var pension: Int    // 政府退休金
-//
-//}
+//        if let data = try? Data(contentsOf: url) {
+//            let x = try? JSONDecoder().decode(PlayerData.self, from: data)
+//        }
+//    }
+}
