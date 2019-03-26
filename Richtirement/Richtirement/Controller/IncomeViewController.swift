38a0c877dce5f9aa4bf5e0f23adb8659abb0a07d
YoCheng <xyox860224@hotmail.com>
2019-03-27 02:12:08 +0800

    Good income chart view.

--- Richtirement/Richtirement/Controller/IncomeViewController.swift
+++ Richtirement/Richtirement/Controller/IncomeViewController.swift
@@ -11,10 +11,15 @@ import Charts
 
 class IncomeViewController: UIViewController {
     @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var incomeTextView: UILabel!
+    @IBOutlet weak var outgoingTextview: UILabel!
+    @IBOutlet weak var nthYearTextView: UILabel!
+    @IBOutlet weak var allMoneyTextView: UILabel!
     
-    let year = 5
-    let incomeVals: [Double] = [0, 50, 22, 29]
-    let outgoingVals: [Double] = [18, 22, 19, 23]
+    let nowYear = 3
+    let allYear = 9
+    let incomeVals: [Double] = [0, 50, 22, 29, 12,22,33,55,11]
+    let outgoingVals: [Double] = [18, 22, 19, 23,22,33,55,11,33]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +28,13 @@ class IncomeViewController: UIViewController {
     
     func setChart() {
         var inEntries: [ChartDataEntry] = []
-        for i in 1..<year {
+        for i in 1..<allYear {
             let dataEntry = ChartDataEntry(x: Double(i), y: incomeVals[i - 1])
             inEntries.append(dataEntry)
         }
         
         var outEntries: [ChartDataEntry] = []
-        for i in 1..<year {
+        for i in 1..<allYear {
             let dataEntry = ChartDataEntry(x: Double(i), y: outgoingVals[i - 1])
             outEntries.append(dataEntry)
         }
@@ -40,6 +45,9 @@ class IncomeViewController: UIViewController {
         inDSet.fillAlpha = 1
         inDSet.drawFilledEnabled = true
         inDSet.drawCirclesEnabled = false
+        inDSet.valueFont = NSUIFont(name: "NotoSansCJKtc-Medium", size: 19)!
+        inDSet.valueColors = [UIColor(red: 255/255, green: 216/255, blue: 113/255, alpha: 1)]
+        inDSet.valueFormatter = IntVChartFormatter()
         
         let outDSet = LineChartDataSet(values: outEntries, label: "支出")
         outDSet.colors = [ NSUIColor(red: 255/255, green: 96/255, blue: 96/255, alpha: 1) ]
@@ -47,6 +55,9 @@ class IncomeViewController: UIViewController {
         outDSet.fillAlpha = 0.7
         outDSet.drawFilledEnabled = true
         outDSet.drawCirclesEnabled = false
+        outDSet.valueFont = NSUIFont(name: "NotoSansCJKtc-Medium", size: 19)!
+        outDSet.valueColors = [UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)]
+        outDSet.valueFormatter = IntVChartFormatter()
         
         let data = LineChartData()
         data.addDataSet(inDSet)
@@ -56,18 +67,8 @@ class IncomeViewController: UIViewController {
         lineChartView.chartDescription?.text = ""
         lineChartView.noDataText = "Loading Data"
         lineChartView.backgroundColor = UIColor.clear
-        
         lineChartView.drawGridBackgroundEnabled = false
-        
-        lineChartView.rightAxis.enabled = false
-        lineChartView.leftAxis.enabled = true
-        
-        lineChartView.dragEnabled = true
-        lineChartView.doubleTapToZoomEnabled = false
-        lineChartView.pinchZoomEnabled = true
-        lineChartView.highlightPerTapEnabled = false
-        lineChartView.highlightPerDragEnabled = false
-        
+
         let xAxis = lineChartView.xAxis
         xAxis.enabled = true
         xAxis.labelPosition = .bottom
@@ -75,8 +76,13 @@ class IncomeViewController: UIViewController {
         xAxis.labelTextColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
         xAxis.gridColor = UIColor(red: 120/255, green: 120/255, blue: 120/255, alpha: 1)
         xAxis.drawGridLinesEnabled = true
-        xAxis.valueFormatter = IntChartFormatter()
+        xAxis.valueFormatter = IntAChartFormatter()
+        xAxis.spaceMin = 0
+        xAxis.granularity = 1
+        xAxis.yOffset = 20
         
+        lineChartView.rightAxis.enabled = false
+        lineChartView.leftAxis.enabled = true
         let leftAxis = lineChartView.leftAxis
         leftAxis.enabled = true
         leftAxis.spaceTop = 1
@@ -84,23 +90,58 @@ class IncomeViewController: UIViewController {
         leftAxis.labelTextColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
         leftAxis.gridColor = UIColor(red: 120/255, green: 120/255, blue: 120/255, alpha: 1)
         leftAxis.drawGridLinesEnabled = true
+        leftAxis.valueFormatter = IntAChartFormatter()
         leftAxis.axisMinimum = 0
+        leftAxis.xOffset = 30
         
         let legend = lineChartView.legend
-        legend.enabled = true
+        legend.enabled = false
+        
+        let xTitle = UILabel(frame: CGRect(x: 45, y: lineChartView.frame.height - 4, width: 20, height: 20))
+        xTitle.text = "年"
+        xTitle.font = UIFont(name: "NotoSansCJKtc-Medium", size: 19)!
+        xTitle.textColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
+        
+        let yTitle = UILabel(frame: CGRect(x: 45, y: 0, width: 20, height: 20))
+        yTitle.text = "萬"
+        yTitle.font = UIFont(name: "NotoSansCJKtc-Medium", size: 19)!
+        yTitle.textColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
+        
+        lineChartView.dragEnabled = true
+        lineChartView.doubleTapToZoomEnabled = false
+        lineChartView.pinchZoomEnabled = false
+        lineChartView.highlightPerTapEnabled = false
+        lineChartView.highlightPerDragEnabled = false
+        
+        lineChartView.addSubview(xTitle)
+        lineChartView.addSubview(yTitle)
+        lineChartView.extraTopOffset = 50
+        lineChartView.setVisibleXRange(minXRange: 3.001, maxXRange: 3.001)
+        lineChartView.moveViewToX(Double(nowYear) - 0.55 - 2)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-
+    @IBAction func confirmBtn_Click(_ sender: Any) {
+        performSegue(withIdentifier: "toAsset", sender: nil)
     }
 }
 
-class IntChartFormatter: NSObject, IAxisValueFormatter {
+class IntVChartFormatter: NSObject, IValueFormatter {
+    func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+        if Double(Int(value)) == value {
+            return String(Int(value))
+            
+        }
+        return ""
+    }
+}
+
+
+class IntAChartFormatter: NSObject, IAxisValueFormatter {
     public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
         if Double(Int(value)) == value {
             return String(Int(value))
+            
         }
-    
         return ""
     }
 }
