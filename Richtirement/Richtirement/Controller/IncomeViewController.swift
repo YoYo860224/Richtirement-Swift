c23e9f33289fb97af480ae8fe3aa58529241b3c6
YoCheng <xyox860224@hotmail.com>
2019-03-26 15:05:53 +0800

    add pod charts.

---
+++ Richtirement/Richtirement/Controller/IncomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeViewController.swift
+//  Richtirement
+//
+//  Created by 陳祐丞 on 2019/3/26.
+//  Copyright © 2019 陳祐丞. All rights reserved.
+//
+
+import UIKit
+
+class IncomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
