5bb6ed10c70e5069c7afa9585c41357353574e9f
YoCheng <xyox860224@hotmail.com>
2019-03-27 10:44:58 +0800

    BG in Asset.

---
+++ Richtirement/Richtirement/Controller/AssetConfirmViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AssetConfirmViewController.swift
+//  Richtirement
+//
+//  Created by 陳祐丞 on 2019/3/27.
+//  Copyright © 2019 陳祐丞. All rights reserved.
+//
+
+import UIKit
+
+class AssetConfirmViewController: UIViewController {
+    @IBOutlet weak var topBGView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUI() {
+        let gradient = CAGradientLayer()
+        gradient.frame =  CGRect(origin: CGPoint.zero, size: self.topBGView.frame.size)
+        gradient.colors = [
+            UIColor(red: 57/255, green: 57/255, blue: 57/255, alpha: 1.0).cgColor,
+            UIColor(red: 41/255, green: 41/255, blue: 41/255, alpha: 1.0).cgColor
+        ]
+        gradient.locations = [0.0, 1.0]
+        topBGView.layer.insertSublayer(gradient, at: 0)
+    }
+}
